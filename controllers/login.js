3a06eb747c2ec4a3450f7f18b0afa7ced9944967
chestnut1717 <psh56750855@gmail.com>
2023-07-24 00:07:38 +0900

    (revise) domain

--- controllers/login.js
+++ controllers/login.js
@@ -46,7 +46,6 @@ module.exports = {
               {
                 sameSite: 'none',
                 secure: true,
-                domain: ".5c43-221-147-91-218.ngrok-free.app"
               }
               );
               // res.header('Access-Control-Allow-Origin', '172.30.1.9');
